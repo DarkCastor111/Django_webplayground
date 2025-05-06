8f32dcb9941c9ec9c0b750392b231ccc31fb6d3d
DarkCastor111 <tempo.cat13@gmail.com>
2025-05-06 13:44:29 +0100

    Version 002 Primeras CBV

--- webplayground/core/views.py
+++ webplayground/core/views.py
@@ -1,7 +1,19 @@
+from django.views.generic.base import TemplateView
 from django.shortcuts import render
 
-def home(request):
-    return render(request, "core/home.html")
+class InicioPageView(TemplateView):
+    template_name = "core/home.html"
 
-def sample(request):
-    return render(request, "core/sample.html")
+    '''
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["titulo_pagina"] = "Mi super Web Playground"
+        return context
+    '''
+    
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name, {"titulo_pagina":"Mi giga Web Playground"})
+
+
+class SamplePageView(TemplateView):
+    template_name = "core/sample.html"
