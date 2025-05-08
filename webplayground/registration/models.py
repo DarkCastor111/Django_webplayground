ce71b01c95ad591bd97d2708c244dbcde74d2ebd
DarkCastor111 <tempo.cat13@gmail.com>
2025-05-08 11:33:55 +0100

    Version 011 Signals

--- webplayground/registration/models.py
+++ webplayground/registration/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
 class Profile(models.Model):
@@ -7,3 +9,9 @@ class Profile(models.Model):
     avatar = models.ImageField(upload_to="profiles", null=True, blank=True)
     bio = models.TextField(null=True, blank=True)
     link = models.URLField(null=True, blank=True)
+
+@receiver(post_save, sender=User)
+def asegurar_existencia_perfil(sender, instance, **kwargs):
+    if kwargs.get('created', False):
+        Profile.objects.get_or_create(user=instance)
+        print("Creación Usuario y su Perfil enlazado")
