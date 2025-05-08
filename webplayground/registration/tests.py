bab751b29ce6b2037ebe69884f65c495e420c8ee
DarkCastor111 <tempo.cat13@gmail.com>
2025-05-08 11:53:32 +0100

    Version 012 Introducción Testing

--- webplayground/registration/tests.py
+++ webplayground/registration/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+# python manage.py test registration
+class PerfilTestCase(TestCase):
+    def setUp(self):
+        User.objects.create_user('nombre_test','correo_toto5@tata.com', 'cred_Hell3210', )
+    
+    def test_existencia_perfil(self):
+        existe = Profile.objects.filter(user__username='nombre_test').exists()
+        self.assertEqual(existe, True)
