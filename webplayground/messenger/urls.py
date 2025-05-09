28eda8496f5daea2fb83deb332c1ff04a05482c7
DarkCastor111 <tempo.cat13@gmail.com>
2025-05-09 12:21:42 +0100

    Version 017 Javascript

--- webplayground/messenger/urls.py
+++ webplayground/messenger/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HiloListView, HiloDetailView
+from .views import HiloListView, HiloDetailView, anadir_mensaje
 
 messenger_patterns = ([
     path('', HiloListView.as_view(), name='hlist'),
     path('hilo/<int:pk>/', HiloDetailView.as_view(), name='hdetail'),
+    path('hilo/<int:p_pk>/anadir', anadir_mensaje, name='hadd'),
 ], 'messenger')
--- webplayground/messenger/views.py
+++ webplayground/messenger/views.py
@@ -3,8 +3,9 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic import TemplateView
 
-from .models import Hilo
-from django.http import Http404
+from .models import Hilo, Mensaje
+from django.http import Http404, JsonResponse
+from django.shortcuts import get_object_or_404
 
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
@@ -33,3 +34,17 @@ class HiloDetailView(DetailView):
         if self.request.user not in hilo.users.all():
             raise Http404()
         return hilo
+    
+def anadir_mensaje(request, p_pk):
+    json_respuesta = {'creada':False}
+    if request.user.is_authenticated:
+        cnt = request.GET.get('contenido', None)
+        if cnt:
+            hilo = get_object_or_404(Hilo, pk=p_pk)
+            mensaje = Mensaje.objects.create(emisor=request.user, contenido=cnt)
+            hilo.mensajes.add(mensaje)
+            json_respuesta["creada"] = True
+    else:
+        raise Http404("Usuario no identificado")
+
+    return JsonResponse(json_respuesta)
